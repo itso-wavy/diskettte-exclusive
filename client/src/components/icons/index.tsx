879feb9b7b1b71d50f9ff912ef4245dd2ef07b1b
itso-wavy <you.meet.vvavy@gmail.com>
2024-05-26 16:24:00 +0900

    feat: svg 파일들 Icon 컴포넌트 내에서 import하여 관리

--- client/src/components/icons/index.tsx
+++ client/src/components/icons/index.tsx
@@ -1,12 +1,18 @@
 import Settings from './settings.svg?react';
-import Sun from './sun.svg?react';
-import Moon from './moon.svg?react';
 import ArrowLeft from './arrow-left.svg?react';
 
 import Home from './home.svg?react';
 import Search from './search.svg?react';
 import Post from './post.svg?react';
 import Bookmark from './bookmark.svg?react';
+
+import Sun from './sun.svg?react';
+import Moon from './moon.svg?react';
+import LogIn from './log-in.svg?react';
+import LogOut from './log-out.svg?react';
+import Profile from './smile.svg?react';
+import Join from './users.svg?react';
+import Diskette from './diskette.svg?react';
 
 import Heart from './heart.svg?react';
 
@@ -18,6 +24,14 @@ const Icon = {
   Search,
   Post,
   Bookmark,
+  Sun,
+  Moon,
+  LogIn,
+  LogOut,
+  Profile,
+  Join,
+  Diskette,
+
   Heart,
 };
 
